9699eea2e99f46d8d3f2fb070675df4f98f4ee0a
stefano.spinelli <stefanospinelli150896@gmail.com>
2024-03-14 22:12:08 +0100

    added info sections

--- lib/info_page.dart
+++ lib/info_page.dart
@@ -7,15 +7,52 @@ class InfoPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Info'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
           children: [
-            Text('App Information'),
-            // Add more Text widgets for additional information
+            _buildSectionTitle('App Info'),
+            _buildSectionContent('This app converts WEBM files to MP4.'),
+            _buildDivider(),
+            _buildSectionTitle('Instructions'),
+            _buildSectionContent('1. Pick one or multiple WEBM files.\n'
+                '2. Click the "Convert" button to start the conversion.'),
+            _buildDivider(),
+            _buildSectionTitle('Developer Info'),
+            _buildSectionContent('Developer: [Your Name]\n'
+                'GitHub: [Your GitHub Link]\n'
+                'Twitter: [Your Twitter Link]'),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildSectionTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSectionContent(String content) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Text(content),
+    );
+  }
+
+  Widget _buildDivider() {
+    return Divider(
+      color: Colors.black,
+      height: 20,
+      thickness: 2,
     );
   }
 }
