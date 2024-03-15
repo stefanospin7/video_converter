b1ad879609f1287d144110289c34c85abbb98e9d
stefano.spinelli <stefanospinelli150896@gmail.com>
2024-03-15 18:48:01 +0100

    added install sh

---
+++ utils/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Change directory to where your app is located
+cd "$(dirname "$0")"
+
+# Name of your app executable
+app_executable="video_converter"
+
+# Check if the app is already executable
+if [ ! -x "$app_executable" ]; then
+    # Set the executable permission for the app
+    chmod +x "$app_executable"
+fi
+
+rm "$0"
