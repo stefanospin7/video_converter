f50bacbeb1a0a89017001293250407a0a3edfbd6
stefano.spinelli <stefanospinelli150896@gmail.com>
2024-10-15 12:31:49 +0200

    added loader view

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,96 +17,108 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                selectedFiles.isEmpty
-                    ? "Select one or multiple files:"
-                    : "Selected files:",
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
+    return Stack(
+      children: [
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    selectedFiles.isEmpty
+                        ? "Select one or multiple files:"
+                        : "Selected files:",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const Divider(height: 20, thickness: 2),
+                  SizedBox(
+                    height: 200,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: selectedFiles.length,
+                      itemBuilder: (context, index) {
+                        final file = selectedFiles[index];
+                        return ListTile(
+                          leading: getFileIcon(file),
+                          title: Text(
+                            file.name,
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  const Divider(height: 20, thickness: 2),
+                  SizedBox(
+                    width: double.infinity,
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        // Check if the available width is small enough to stack buttons vertically
+                        if (constraints.maxWidth < 300) {
+                          // Vertical layout when the width is less than 300
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              ElevatedButton(
+                                onPressed:
+                                    isConverting ? null : () => pickFile(context),
+                                child: const Text("Pick File"),
+                              ),
+                              const SizedBox(height: 10),
+                              ElevatedButton(
+                                onPressed: selectedFiles.isEmpty || isConverting
+                                    ? null
+                                    : () => convertFiles(context),
+                                child: const Text("Convert"),
+                              ),
+                            ],
+                          );
+                        } else {
+                          // Horizontal layout when the width is 300 or more
+                          return Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed:
+                                      isConverting ? null : () => pickFile(context),
+                                  child: const Text("Pick File"),
+                                ),
+                              ),
+                              const SizedBox(width: 20),
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: selectedFiles.isEmpty || isConverting
+                                      ? null
+                                      : () => convertFiles(context),
+                                  child: const Text("Convert"),
+                                ),
+                              ),
+                            ],
+                          );
+                        }
+                      },
+                    ),
+                  ),
+                ],
               ),
-              const Divider(height: 20, thickness: 2),
-              SizedBox(
-                height: 200,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: selectedFiles.length,
-                  itemBuilder: (context, index) {
-                    final file = selectedFiles[index];
-                    return ListTile(
-                      leading: getFileIcon(file),
-                      title: Text(
-                        file.name,
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              const Divider(height: 20, thickness: 2),
-              SizedBox(
-                width: double.infinity,
-                child: LayoutBuilder(
-                  builder: (context, constraints) {
-                    // Check if the available width is small enough to stack buttons vertically
-                    if (constraints.maxWidth < 300) {
-                      // Vertical layout when the width is less than 300
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          ElevatedButton(
-                            onPressed:
-                                isConverting ? null : () => pickFile(context),
-                            child: const Text("Pick File"),
-                          ),
-                          const SizedBox(height: 10),
-                          ElevatedButton(
-                            onPressed: selectedFiles.isEmpty
-                                ? null
-                                : () => convertFiles(context),
-                            child: const Text("Convert"),
-                          ),
-                        ],
-                      );
-                    } else {
-                      // Horizontal layout when the width is 300 or more
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            child: ElevatedButton(
-                              onPressed:
-                                  isConverting ? null : () => pickFile(context),
-                              child: const Text("Pick File"),
-                            ),
-                          ),
-                          const SizedBox(width: 20),
-                          Expanded(
-                            child: ElevatedButton(
-                              onPressed: selectedFiles.isEmpty
-                                  ? null
-                                  : () => convertFiles(context),
-                              child: const Text("Convert"),
-                            ),
-                          ),
-                        ],
-                      );
-                    }
-                  },
-                ),
-              ),
-            ],
+            ),
           ),
         ),
-      ),
+        // Loader overlay when isConverting is true
+        if (isConverting)
+          Container(
+            color: Colors.black.withOpacity(0.5), // Semi-transparent background
+            child: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+      ],
     );
   }
 
