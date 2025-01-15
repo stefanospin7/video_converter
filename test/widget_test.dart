e095dada00dec4f24cf5f1ce2e1b9f6687e22e33
stefano.spinelli <stefanospinelli150896@gmail.com>
2025-01-16 00:26:20 +0100

    changed name video_converter to webm_converter

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:video_converter/main.dart';
+import 'package:webm_converter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
